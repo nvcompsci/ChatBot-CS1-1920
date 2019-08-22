a8fcb075de9eb720608a92e7c32913d7d5efda44
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2019-08-22 08:48:09 -0400

    getters, setters, ChatBot.eat(), class Food

--- ChatBot.java
+++ ChatBot.java
@@ -11,21 +11,51 @@ package chatbotapp;
  */
 public class ChatBot {
     //Fields (object variables)
-    String name;
-    int age;
-    double weight;
+    private String name;
+    private int age;
+    private double weight;
     
     //Constructor (Creates the object)
     public ChatBot() {
         name = "Bob";
         age = 4;
-        
+        weight = 50;
     }
     
     //Getters and Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) 
+            this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    
     
     //Methods (What the object does)
     public String respond(String prompt) {
         return "I don't understand yet.";
+    }
+    
+    public void eat(Food food) {
+        weight += food.weight;
     }
 }
---
+++ Food.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatbotapp;
+
+/**
+ *
+ * @author jword
+ */
+public class Food {
+    //Fields
+    
+    //Constructor
+    
+    //Getters and Setters
+    
+    //Methods
+    
+}
